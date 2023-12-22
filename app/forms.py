5ba1d14a7a44d86a4ec32f73b1a6bbf8f6791d9c
hiroshipencil <143366259+hiroshipencil@users.noreply.github.com>
2023-12-22 22:40:43 +0900

    Update forms.py
    機種の複数選択

--- app/forms.py
+++ app/forms.py
@@ -13,9 +13,36 @@ class ItemForm(forms.ModelForm):
     class Meta:
         model = Item
         fields = '__all__'
+        #↓機種をチェックにする？
+        widgets = {
+            'sample_2': forms.CheckboxSelectMultiple,
+        }
+        #↑機種をチェックにする？
 
         # 以下のフィールド以外が入力フォームに表示される
         # AutoField
         # auto_now=True
         # auto_now_add=Ture
         # editable=False
+        
+class SampleForm(forms.Form):
+    sample_1_choice = (
+        ('GCC', 'GCC'),
+        ('SEI', 'SEI'),
+        ('ROC', 'ROC'),
+        ('WAZER', 'WAZER'),
+    )
+
+    sample_1 = forms.ChoiceField(
+        label='メーカー',
+        choices=sample_1_choice,
+        widget=forms.Select(attrs={'id': 'sample-1'}),
+        required=False,
+    )
+
+    sample_2 = forms.ChoiceField(
+        label='機種',
+        choices=(),
+        widget=forms.Select(attrs={'id': 'sample-2'}),
+        required=False,
+    )
